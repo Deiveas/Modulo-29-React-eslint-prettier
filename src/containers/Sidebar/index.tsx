61c4d2801840bdf687c8d474a864501f82b0d241
Deive Silva <deiveas@gmail.com>
2024-05-13 12:04:17 -0300

    configurado componentes paragrafo, titulo, projetos, sidebar e sobre

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/deiveas.png" alt="" />
+    <Titulo fontSize={20}>Deive Silva</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,16 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, corrupti
+      consequatur corporis ex sit voluptatum ad unde praesentium nobis quaerat
+      quis porro aspernatur optio dolorum doloribus. Eaque molestiae impedit
+      laboriosam.
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
