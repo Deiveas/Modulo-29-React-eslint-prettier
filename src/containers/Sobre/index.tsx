41e4800b87eeaf64dd61d65b7ce709b62916b373
Deive Silva <deiveas@gmail.com>
2024-05-14 10:25:24 -0300

    seção sobre mim finalizada

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,15 +1,19 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
     <Paragrafo tipo="secundario">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, corrupti
-      consequatur corporis ex sit voluptatum ad unde praesentium nobis quaerat
-      quis porro aspernatur optio dolorum doloribus. Eaque molestiae impedit
-      laboriosam.
+      Desenvolvedor Front-End altamente motivado e apaixonado por criar
+      experiências de usuário excepcionais. Com uma sólida formação no
+      desenvolvimento de web sites.
     </Paragrafo>
+    <GithubSecao>
+      {/* <img src="https://github-readme-stats.vercel.app/api?username=Deiveas&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" /> */}
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Deiveas&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
